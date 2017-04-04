1f29b5f999615c22358b314773bf6b3f6fffae1f
Chase Sang <chase.sang@gmail.com>
2017-04-04 13:34:42 -0700

    added bootstrap

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :category
-  has_many :reviews
+  has_many :reviews,dependent: :destroy
   validates :title, presence: true, uniqueness: {case_sensitive: false}
   validates(:price, { presence: true,
                             numericality: { greater_than: 0 }})
